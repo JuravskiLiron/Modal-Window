d46484948c5db19e29f4b79ed77413c25787899d
JuravskiLiron <juravskliron@gmail.com>
2024-05-26 22:34:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.scss';
+
+function App() {
+
+  const [open, setOpen] = React.useState(false);
+
+
+  return (
+    <div className="App">
+      <button onClick = {() => setOpen(true)} className="open-modal-btn">Log In</button>
+      {
+        open && (
+           
+            <div className="overlay">
+              <div className="modal">
+                <svg onClick = {() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                  <title />
+                  <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+                </svg>
+                <img class = "LoginImg" src="https://support.unoh.edu/media/images/proxy-sign-in.width-500.png" />
+              </div>
+            </div> 
+            
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
